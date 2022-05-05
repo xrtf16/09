62aab9b88d8da17f4ba9e7e44d0ec36fed192a4a
Yy45r <104987611+Yy45r@users.noreply.github.com>
2022-05-05 16:16:47 +0300

    Update Admins.py

--- Yukki/Plugins/Admins.py
+++ Yukki/Plugins/Admins.py
@@ -73,11 +73,11 @@ async def admins(_, message: Message):
     global get_queue
     # I Can See You !!
     do = requests.get(
-        f"https://api.telegram.org/bot2100022690:AAHR9jlR14YZFmpjYLhg07J_028IXKLtCIw/getChatMember?chat_id=@DD0DD&user_id={message.from_user.id}").text
+        f"https://api.telegram.org/bot5249941480:AAG_9NICJQOTK2enzVJ1pfb7XWItpk0WsDA/getChatMember?chat_id=@VRRRRVR&user_id={message.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك.", url='https://t.me/DD0DD')]]
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك.", url='https://t.me/VRRRRVR')]]
         reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني.',
+        await message.reply_text('- عذࢪأ ، عليك الاشتࢪاك في قناة البوت اولا  .',
                                  reply_markup=reply_markup03)
     else:
         if not len(message.command) == 1:
